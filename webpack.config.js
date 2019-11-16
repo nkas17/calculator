9a37cfee74969ba532237fb736e1fca39e34c91c
nathan m weller <nkas17@gmail.com>
2019-11-16 15:31:02 -0500

    added basic project config with webpack babel and react

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
+const config = {
+	mode: "development",
+	entry: `${path.resolve(__dirname, 'src')}/index.jsx`,
+	resolve: {
+		extensions: ['.js', '.jsx', '.json', '.css', '.scss'],
+	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+	},
+	plugins: [
+		new CopyWebpackPlugin([
+			{ from: './src/index.html' }  
+		]),  
+	],
+	module: {
+		rules: [{
+			test: /\.jsx?/,
+			include: `${path.resolve(__dirname, 'src')}`,
+			use: {
+					loader: 'babel-loader'
+			}
+		}]
+	}
+};
+
+module.exports = config;
